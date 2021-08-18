9a4e6ca2c3c17327ed3ef9bd91991e6391d87048
medium0fry <medium0fry@gmail.com>
2021-08-18 10:42:46 -0400

    created renameable buttons

---
+++ src/components/RenamableButton.vue
@@ -0,0 +1,31 @@
+<template>
+	<input v-if="renaming" type="text" :value="name" @change="handleChange" />
+	<button v-else @dblclick="toggleRename">{{ name }}</button>
+</template>
+
+<script>
+export default {
+	name: 'RenameableButton',
+	props: {
+		name: String,
+		location: Object,
+	},
+	methods: {
+		toggleRename() {
+			this.renaming = !this.renaming;
+		},
+		handleChange(e) {
+			console.log(e);
+		},
+	},
+	data() {
+		return {
+			name: '',
+			working: '',
+			renaming: false,
+		};
+	},
+};
+</script>
+
+<style scoped></style>
